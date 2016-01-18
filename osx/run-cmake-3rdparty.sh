8fcf9396dc0eb2ae1af6327cc030dbb9494c76c7
Boudewijn Rempt <boud@valdyas.org>
2016-01-18 08:23:09 +0100

    Add the scripts I use to build on OSX
    They aren't generic yet...

--- osx/run-cmake-3rdparty.sh
+++ osx/run-cmake-3rdparty.sh
@@ -1,1 +1,8 @@
-cmake ../krita/3rdparty/ -DCMAKE_INSTALL_PREFIX=/Users/boud/dev/i -DEXTERNALS_DOWNLOAD_DIR=/Users/boud/dev/d  -DINSTALL_ROOT=/Users/boud/dev/i
+#export MACOSX_DEPLOYMENT_TARGET=10.7
+#export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk
+cmake ../krita/3rdparty/  \
+    -DCMAKE_INSTALL_PREFIX=/Users/boud/dev/i \
+    -DEXTERNALS_DOWNLOAD_DIR=/Users/boud/dev/d  \
+    -DINSTALL_ROOT=/Users/boud/dev/i 
+ #   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+ #   -CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk
